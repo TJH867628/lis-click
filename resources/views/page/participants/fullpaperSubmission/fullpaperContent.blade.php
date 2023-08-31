2642cd373ca55e47aeffc0e01dcef4f31e603548
TJH <jinghengtan762@gmail.com>
2023-08-31 22:20:51 +0800

    debug

--- resources/views/page/participants/fullpaperSubmission/fullpaperContent.blade.php
+++ resources/views/page/participants/fullpaperSubmission/fullpaperContent.blade.php
@@ -176,3 +176,6 @@
                     </div>
                 </div>
             </header>
+        
+        
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
