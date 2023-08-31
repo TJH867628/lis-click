3c85e9fbe45111a2c3f8eac3dfde71b7a70cdb25
TJH <jinghengtan762@gmail.com>
2023-08-31 22:51:57 +0800

    Update paymentQRContent.blade.php

--- resources/views/page/JK_Bendahari/paymentQR/paymentQRContent.blade.php
+++ resources/views/page/JK_Bendahari/paymentQR/paymentQRContent.blade.php
@@ -14,9 +14,9 @@
                 <form action="{{ route('uploadNewPaymentQR') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     UPLOAD NEW QR CODE
-                    <input type="file" name="image" accept=".jpeg,.jpg,.png">
                     <label for="details">Please type the payment details below</label><br>
-                    <textarea style="width: 500px; height:200px;" name="details" type="text">{{ $qrCode->masterdata_details }}</textarea><br>
+                    <textarea style="width: 500px; height:200px;" name="details" type="text"></textarea><br>
+                    <input style="margin:auto;" type="file" name="image" accept=".jpeg,.jpg,.png">
                     <button type="submit">Upload</button>
                 </form>
             @else
