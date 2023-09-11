caa8e2557e6514757f8f6f814a982453500d53f6
TJH <jinghengtan762@gmail.com>
2023-09-11 14:48:37 +0800

    Update FE 11/9/2023
    -Presentation Schedule

--- resources/views/page/participants/presentationSchedule/presentationScheduleContent.blade.php
+++ resources/views/page/participants/presentationSchedule/presentationScheduleContent.blade.php
@@ -5,142 +5,365 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet">
     <link href="/css/styles.css" rel="stylesheet">
     <style>
-        body {
-            background-color: #f8f9fa;
-            font-family: Arial, sans-serif;
+
+        * {
+            margin: 0;
+            padding: 0;
+
+            box-sizing: border-box;
+            font-family: sans-serif;
         }
 
-        h1 {
-            text-align: center;
-            margin-top: 50px;
-            margin-bottom: 30px;
+        body {
+            min-height: 100vh;
+            background: url("../images/sunsetBackground.jpg") center / cover;
+            display: flex;
+            justify-content: center;
+        }
+
+        main.table {
+            margin-top: 3%;
+            width: 82vw;
+            height: 70vh;
+            background-color: #fff5;
+            backdrop-filter: blur(7px);
+            box-shadow: 0 .4rem .8rem #0005;
+            border-radius: .8rem;
+            overflow: hidden;
+        }
+
+        .table__header {
+            width: 100%;
+            height: 15%;
+            background-color: #fff4;
+            padding: 1rem 1rem;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        .table__header .input-group {
+            width: 35%;
+            height: 100%;
+            background-color: #fff5;
+            padding: 0 .8rem;
+            border-radius: 2rem;
+
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            transition: .2s;
+        }
+
+        .table__header .input-group:hover {
+            width: 45%;
+            background-color: #fff8;
+            box-shadow: 0 .1rem .4rem #0002;
+        }
+
+        .table__header .input-group img {
+            width: 1.2rem;
+            height: 1.2rem;
+        }
+
+        .table__header .input-group input {
+            width: 100%;
+            padding: 0 .5rem 0 .3rem;
+            background-color: transparent;
+            border: none;
+            outline: none;
+        }
+
+        .table__body {
+            width: 95%;
+            max-height: calc(84% - 1.6rem);
+            background-color: #fffb;
+
+            margin: .8rem auto;
+            border-radius: .6rem;
+
+            overflow: auto;
+            overflow: overlay;
+        }
+
+        .table__body::-webkit-scrollbar{
+            width: 0.5rem;
+            height: 0.5rem;
+        }
+
+        .table__body::-webkit-scrollbar-thumb{
+            border-radius: .5rem;
+            background-color: #0004;
+            visibility: hidden;
+        }
+
+        .table__body:hover::-webkit-scrollbar-thumb{ 
+            visibility: visible;
         }
 
         table {
-            margin: auto;
-            margin-top: 10%;
-            margin-bottom: 10%;
+            width: 100%;
+        }
+
+        td img {
+            width: 36px;
+            height: 36px;
+            margin-right: .5rem;
+            border-radius: 50%;
+
+            vertical-align: middle;
+        }
+
+        table, th, td {
             border-collapse: collapse;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-            background-color: white;
+            padding: 1rem;
+            text-align: left;
         }
 
-        th, td {
-            padding: 10px;
-            width: 10%;
-            text-align: center;
-            border: 1px solid #dee2e6;
-        }
-
-
-        th {
-            background-color: #343a40;
-            color: white;
-        }
-
-        form {
-            color: black;
-            margin: auto;
-            width: 500px;
-            padding: 20px;
-            border: 1px solid #dee2e6;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-            background-color: white;
-        }
-
-        input[type="text"], textarea {
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #ced4da;
-            border-radius: 4px;
-            box-sizing: border-box;
-        }
-
-        input[type="datetime-local"] {
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #ced4da;
-            border-radius: 4px;
-            box-sizing: border-box;
-            margin-bottom: 20px;
-        }
-
-        button {
-            background-color: #007bff;
-            color: white;
-            padding: 7px 25px;
-            border: none;
-            border-radius: 4px;
+        thead th {
+            position: sticky;
+            top: 0;
+            left: 0;
+            background-color: #d5d1defe;
             cursor: pointer;
+            text-transform: capitalize;
         }
 
-        .deleteButton {
-            background-color: red;
-            color: white;
-            padding: 10px 20px;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-            text-decoration: none;
+        tbody tr:nth-child(even) {
+            background-color: #0000000b;
         }
 
-        button:hover {
-            background-color: #0069d9;
-        }
-        
-        .title{
-            margin-top: 10%;
-        }
-        
-        .btn, .download{
-            text-align: center;
-            width: max-content;
+        tbody tr {
+            --delay: .1s;
+            transition: .5s ease-in-out var(--delay), background-color 0s;
         }
 
-        label{
-            margin: 0;
+        tbody tr.hide {
+            opacity: 0;
+            transform: translateX(100%);
+        }
+
+        tbody tr:hover {
+            background-color: #fff6 !important;
+        }
+
+        tbody tr td,
+        tbody tr td p,
+        tbody tr td img {
+            transition: .2s ease-in-out;
+        }
+
+        tbody tr.hide td,
+        tbody tr.hide td p {
             padding: 0;
-            border: none;
-            font-size: 16px;
-            font-weight: bold;
-            color: #343a40;
+            font: 0 / 0 sans-serif;
+            transition: .2s ease-in-out .5s;
+        }
+
+        tbody tr.hide td img {
+            width: 0;
+            height: 0;
+            transition: .2s ease-in-out .5s;
+        }
+
+        .status {
+            padding: .4rem 0;
+            border-radius: 2rem;
+            text-align: center;
+        }
+
+        .status.delivered {
+            background-color: #86e49d;
+            color: #006b21;
+        }
+
+        .status.cancelled {
+            background-color: #d893a3;
+            color: #b30021;
+        }
+
+        .status.pending {
+            background-color: #ebc474;
+        }
+
+        .status.shipped {
+            background-color: #6fcaea;
+        }
+
+
+        @media (max-width: 1000px) {
+            td:not(:first-of-type) {
+                min-width: 12.1rem;
+            }
+        }
+
+        thead th span.icon-arrow {
+            display: inline-block;
+            width: 1.3rem;
+            height: 1.3rem;
+            border-radius: 50%;
+            border: 1.4px solid transparent;
+            
+            text-align: center;
+            font-size: 1rem;
+            
+            margin-left: .5rem;
+            transition: .2s ease-in-out;
+        }
+
+        thead th:hover span.icon-arrow{
+            border: 1.4px solid #6c00bd;
+        }
+
+        thead th:hover {
+            color: #6c00bd;
+        }
+
+        thead th.active span.icon-arrow{
+            background-color: #6c00bd;
+            color: #fff;
+        }
+
+        thead th.asc span.icon-arrow{
+            transform: rotate(180deg);
+        }
+
+        thead th.active,tbody td.active {
+            color: #6c00bd;
         }
     </style>
 </head>
 <body>
-    <table>
-        <thead>
-            <tr>
-                <th colspan="4">
-                    <h3 style="color: white; text-align:center;">Presentation Schedule</h3>
-                </th>
-            </tr>
-            <tr>
-                <th>Group</th>
-                <th>Time</th>
-                <th>Link</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php
-                $scheduleCount = 0
-            @endphp
-            @foreach($schedule as $eachSchedule)
-                @php
-                    $scheduleCount++
-                @endphp
-            <tr>
-                <td><label type="text" name="group">{{ $eachSchedule->presentationGroup }}</label></td>
-                <td><label type="datetime-local" name="time" >{{ $eachSchedule->presentationTime }}</label></td>
-                <td><label type="text" name="link" >{{ $eachSchedule->presentationLink }}</label></td>
-            </tr>
-            @endforeach
-            @if($scheduleCount == 0)
-            <tr>
-                <td colspan="4" style="color: black;">The Schedule List Is Empty</td>
-            </tr>
-            @endif
-        </tbody>
-    </table>
+    <main class="table">
+        <section class="table__header">
+            <h1>Presentation Schedule</h1>
+            <!-- <div class="input-group">
+                <input type="search" placeholder="Search Data...">
+                <img src="images/search.png" alt="">
+            </div> -->
+        </section>
+        <section class="table__body">
+            <table>
+                <thead>
+                    <tr>
+                        <th> Group</th>
+                        <th> Time</th>
+                        <th> Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- <tr>
+                        <td> 1 </td>
+                        <td> <img src="images/Zinzu Chan Lee.jpg" alt="">Zinzu Chan Lee</td>
+                        <td> Seoul </td>
+                        <td> 17 Dec, 2022 </td>
+                        <td>
+                            <p class="status delivered">Delivered</p>
+                        </td>
+                        <td> <strong> $128.90 </strong></td>
+                    </tr>
+                    <tr>
+                        <td> 2 </td>
+                        <td><img src="images/Jeet Saru.jpg" alt=""> Jeet Saru </td>
+                        <td> Kathmandu </td>
+                        <td> 27 Aug, 2023 </td>
+                        <td>
+                            <p class="status cancelled">Cancelled</p>
+                        </td>
+                        <td> <strong>$5350.50</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 3</td>
+                        <td><img src="images/Sonal Gharti.jpg" alt=""> Sonal Gharti </td>
+                        <td> Tokyo </td>
+                        <td> 14 Mar, 2023 </td>
+                        <td>
+                            <p class="status shipped">Shipped</p>
+                        </td>
+                        <td> <strong>$210.40</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 4</td>
+                        <td><img src="images/Alson GC.jpg" alt=""> Alson GC </td>
+                        <td> New Delhi </td>
+                        <td> 25 May, 2023 </td>
+                        <td>
+                            <p class="status delivered">Delivered</p>
+                        </td>
+                        <td> <strong>$149.70</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 5</td>
+                        <td><img src="images/Sarita Limbu.jpg" alt=""> Sarita Limbu </td>
+                        <td> Paris </td>
+                        <td> 23 Apr, 2023 </td>
+                        <td>
+                            <p class="status pending">Pending</p>
+                        </td>
+                        <td> <strong>$399.99</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 6</td>
+                        <td><img src="images/Alex Gonley.jpg" alt=""> Alex Gonley </td>
+                        <td> London </td>
+                        <td> 23 Apr, 2023 </td>
+                        <td>
+                            <p class="status cancelled">Cancelled</p>
+                        </td>
+                        <td> <strong>$399.99</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 7</td>
+                        <td><img src="images/Alson GC.jpg" alt=""> Jeet Saru </td>
+                        <td> New York </td>
+                        <td> 20 May, 2023 </td>
+                        <td>
+                            <p class="status delivered">Delivered</p>
+                        </td>
+                        <td> <strong>$399.99</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 8</td>
+                        <td><img src="images/Sarita Limbu.jpg" alt=""> Aayat Ali Khan </td>
+                        <td> Islamabad </td>
+                        <td> 30 Feb, 2023 </td>
+                        <td>
+                            <p class="status pending">Pending</p>
+                        </td>
+                        <td> <strong>$149.70</strong> </td>
+                    </tr>
+                    <tr>
+                        <td> 9</td>
+                        <td><img src="images/Alex Gonley.jpg" alt=""> Alson GC </td>
+                        <td> Dhaka </td>
+                        <td> 22 Dec, 2023 </td>
+                        <td>
+                            <p class="status cancelled">Cancelled</p>
+                        </td>
+                        <td> <strong>$249.99</strong> </td>
+                    </tr> -->
+                    @php
+                        $scheduleCount = 0
+                    @endphp
+                    @foreach($schedule as $eachSchedule)
+                        @php
+                            $scheduleCount++
+                        @endphp
+                    <tr>
+                        <td><label type="text" name="group">{{ $eachSchedule->presentationGroup }}</label></td>
+                        <td><label type="datetime-local" name="time" >{{ $eachSchedule->presentationTime }}</label></td>
+                        <td><label type="text" name="link" >{{ $eachSchedule->presentationLink }}</label></td>
+                    </tr>
+                    @endforeach
+                    @if($scheduleCount == 0)
+                    <tr>
+                        <td colspan="4">The Schedule List Is Empty</td>
+                    </tr>
+                    @endif
+                </tbody>
+            </table>
+        </section>
+    </main>
 </body>
 </html>
