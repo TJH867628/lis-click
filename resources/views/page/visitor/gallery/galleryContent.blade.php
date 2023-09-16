d02522d549ce3a361a3d94c6d770011086616f66
chaiyuxuan666 <128983394+chaiyuxuan666@users.noreply.github.com>
2023-09-16 14:42:27 +0800

    update gallery 16/9

--- resources/views/page/visitor/gallery/galleryContent.blade.php
+++ resources/views/page/visitor/gallery/galleryContent.blade.php
@@ -10,108 +10,165 @@
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet"/>
+        <link href="../css/styles.css" rel="stylesheet"/>
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <style>
-        body {
-            background-color: #f7f7f7;
-            text-align: center;
-            padding-top: 50px;
-        }
+            body{
+                background-color: #eaeaea;
+                overflow: hidden;
+            }
+            #container{
+                position: absolute;
+                left:50%;
+                top:50%;
+                transform: translate(-50%,-50%);
+                width:1000px;
+                height:600px;
+                padding:50px;
+                background-color: #f5f5f5;
+                box-shadow: 0 30px 50px #dbdbdb;
+            }
+            #slide{
+                width:max-content;
+                margin-top:50px;
+            }
+            #item{
+                width:200px;
+                height:300px;
+                background-position: 50% 50%;
+                display: inline-block;
+                transition: 0.5s;
+                background-size: contain;
+                background-size: 100% 100%;
+                background-repeat: no-repeat;
+                position: absolute;
+                z-index: 1;
+                top:50%;
+                transform: translate(0,-50%);
+                border-radius: 20px;
+                box-shadow:  0 30px 50px #505050;
+            }
 
-        h1 {
-            padding: 10px;
-            color: black;
-            font-weight: bold;
-        }
-
-        #ImgContainer {
-            max-width: 50%;
-            margin: auto;
-            padding: 50px;
-            background-color: #0000001f;
-            box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-            border-radius: 50px;
-            margin-bottom: 20px;
-        }
-
-        img {
-            max-width: 70%;
-            max-height: 100%;
-            border-radius: 5px;
-            margin-bottom: 10px;
-        }
-
-        h3 {
-            color: #000000;
-            margin-bottom: 5px;
-        }
-
-        p {
-            color: #000000;
-            font-size: 16px;
-            line-height: 1.5;
-            margin-bottom: 15px;
-        }
-
-        /* Add some colors to buttons */
-        .btn {
-            background-color: #007bff;
-            color: #fff;
-            border: none;
-            padding: 8px 16px;
-            border-radius: 4px;
-            transition: background-color 0.3s;
-        }
-
-        .btn:hover {
-            background-color: #0056b3;
-        }
-
-        /* Center the "Gallery Is Empty" message */
-        .empty-message {
-            color: #000000;
-            margin-top: 20%;
-        }
-
-        .galleryContainer{
-            margin-bottom: 10%;
-        }
-
-        #title{
-            font-weight: bold;            
-            margin-top: 20px;
-            text-align: left;
-            font-size: 30px;
-            border-bottom: 3px solid #000000;
-        }
-
-        #description{
-            margin-top: 20px;
-            font-size: 20px;
-            background-color: #f7f7f7;
-            border-radius: 10px;
-        }
+            #item:nth-child(1),
+            #item:nth-child(2){
+                left:0;
+                top:0;
+                transform: translate(0,0);
+                border-radius: 0;
+                width:100%;
+                height:100%;
+                box-shadow: none;
+            }
+            #item:nth-child(3){
+                left:50%;
+            }
+            #item:nth-child(4){
+                left:calc(50% + 220px);
+            }
+            #item:nth-child(5){
+                left:calc(50% + 440px);
+            }
+            #item:nth-child(n+6){
+                left:calc(50% + 660px);
+                opacity: 0;
+            }
+            #item #content{
+                position: absolute;
+                top:50%;
+                left:100px;
+                width:300px;
+                text-align: left;
+                padding:0;
+                color:#eee;
+                transform: translate(0,-50%);
+                display: none;
+                font-family: system-ui;
+            }
+            #item:nth-child(2) #content{
+                display: block;
+                z-index: 11111;
+            }
+            #item #name{
+                color: black;
+                font-size: 40px;
+                font-weight: bold;
+                opacity: 0;
+                animation:showcontent 1s ease-in-out 1 forwards
+            }
+            #item #des{
+                color: black;
+                margin:20px 0;
+                opacity: 0;
+                animation:showcontent 1s ease-in-out 0.3s 1 forwards
+            }
+            #item #button{
+                padding:10px 20px;
+                border:none;
+                opacity: 0;
+                animation:showcontent 1s ease-in-out 0.6s 1 forwards
+            }
+            @keyframes showcontent{
+                from{
+                    opacity: 0;
+                    transform: translate(0,100px);
+                    filter:blur(33px);
+                }to{
+                    opacity: 1;
+                    transform: translate(0,0);
+                    filter:blur(0);
+                }
+            }
+            #buttons{
+                position: absolute;
+                bottom:30px;
+                z-index: 222222;
+                text-align: center;
+                width:100%;
+            }
+            #buttons button{
+                width:50px;
+                height:50px;
+                border-radius: 50%;
+                border:1px solid #555;
+                transition: 0.5s;
+            }#buttons button:hover{
+                background-color: #bac383;
+            }
     </style>
     </head>
-    <body style="text-align: center;">
-    <div class="galleryContainer">
-        @if($gallery->isNotEmpty())
-                <h1>Gallery</h1>
-            @foreach($gallery as $gallery)
-                @if($gallery->visible == true)
-                <div id="ImgContainer">
-                    <img src="{{ $gallery->imageSrc }}">
-                    <h3 id="title">{{ $gallery->title }}</h3>
-                    <p id="description">{{ $gallery->description }}</p>
-                </div>
+    <body>
+    @if($gallery->isNotEmpty())
+    <div id="container">
+        <div id="slide">
+            @foreach($gallery as $galleryItem)
+                @if($galleryItem->visible == true)
+                    <div id="item" style="background-image: url({{ $galleryItem->imageSrc }});">
+                        <div id="content">
+                            <div id="name">{{ $galleryItem->title }}</div>
+                            <div id="des">{{ $galleryItem->description }}</div>
+                        </div>
+                    </div>
                 @endif
             @endforeach
-        @else
-            <h1 style="color: white; margin-top:20%;">Gallery Is Empty </h1>
-        @endif
+        </div>
+        <div id="buttons">
+            <button id="prev"><i class="fa-solid fa-angle-left"></i></button>
+            <button id="next"><i class="fa-solid fa-angle-right"></i></button>
+        </div>
     </div>
-       
-    </body>
+    @endif
+    <script>
+        document.getElementById('next').onclick = function(){
+            let lists = document.querySelectorAll('#item');
+            document.getElementById('slide').appendChild(lists[0]);
+        }
+
+        document.getElementById('prev').onclick = function(){
+            let lists = document.querySelectorAll('#item');
+            document.getElementById('slide').prepend(lists[lists.length - 1]);
+        }
+    </script>
+</body>
 </html>
