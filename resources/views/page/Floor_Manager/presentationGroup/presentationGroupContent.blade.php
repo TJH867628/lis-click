c9b9000e2c776108bc80083950c536b4d5f95f01
chew0420 <chewjiawei123@gmail.com>
2023-09-22 18:38:18 +0800

    22/9

--- resources/views/page/Floor_Manager/presentationGroup/presentationGroupContent.blade.php
+++ resources/views/page/Floor_Manager/presentationGroup/presentationGroupContent.blade.php
@@ -1,7 +1,172 @@
 <html>
     <head>
     <style>
-        body {
+        * {
+        margin: 0;
+        padding: 0;
+
+        box-sizing: border-box;
+        font-family: sans-serif;
+    }
+
+    body {
+        min-height: 100vh;
+        background: url("../images/tblBackground.jpg") center / cover;
+        display: flex;
+        justify-content: center;
+    }
+
+    main.table {
+        margin-top: 5%;
+        width: 70vw !important;
+        height: 70vh;
+        background-color: #fff5;
+        backdrop-filter: blur(7px);
+        box-shadow: 0 .4rem .8rem #0005;
+        border-radius: .8rem;
+        overflow: hidden;
+    }
+
+    .table__header {
+        z-index:0;
+        width: 100%;
+        height: 15%;
+        background-color: #fff4;
+        padding: 1rem 1rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .table__header .input-group {
+        width: 35%;
+        height: 100%;
+        background-color: #fff5;
+        padding: 0 .8rem;
+        border-radius: 2rem;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        transition: .2s;
+    }
+
+    .table__header .input-group:hover {
+        width: 45%;
+        background-color: #fff8;
+        box-shadow: 0 .1rem .4rem #0002;
+    }
+
+    .table__header .input-group img {
+        width: 1.2rem;
+        height: 1.2rem;
+    }
+
+    .table__header .input-group input {
+        width: 100%;
+        padding: 0 .5rem 0 .3rem;
+        background-color: transparent;
+        border: none;
+        outline: none;
+    }
+
+    .table__body {
+        position: relative;
+        width: 90%;
+        max-height: calc(84% - 1.6rem);
+        background-color: #fffb;
+
+        margin: .8rem auto;
+        border-radius: .6rem;
+
+        overflow: auto;
+        overflow: overlay;
+    }
+
+    .table__body::-webkit-scrollbar{
+        width: 0.5rem;
+        height: 0.5rem;
+    }
+
+    .table__body::-webkit-scrollbar-thumb{
+        border-radius: .5rem;
+        background-color: #0004;
+        visibility: hidden;
+    }
+
+    .table__body:hover::-webkit-scrollbar-thumb{ 
+        visibility: visible;
+    }
+
+    table {
+        width: 100%;
+    }
+
+    td img {
+        width: 36px;
+        height: 36px;
+        margin-right: .5rem;
+        border-radius: 50%;
+
+        vertical-align: middle;
+    }
+
+    table, th, td {
+        border-collapse: collapse;
+        padding: 1rem;
+        text-align: left;
+    }
+
+    th {
+        background-color: #d5d1defe !important;
+    }
+
+    thead th {
+        position: sticky;
+        top: 0;
+        left: 0;
+        cursor: pointer;
+        text-transform: capitalize;
+    }
+
+    tbody tr:nth-child(even) {
+        background-color: #0000000b;
+    }
+
+    tbody tr {
+        --delay: .1s;
+        transition: .5s ease-in-out var(--delay), background-color 0s;
+    }
+
+    tbody tr.hide {
+        opacity: 0;
+        transform: translateX(100%);
+    }
+
+    tbody tr:hover {
+        background-color: #fff6 !important;
+    }
+
+    tbody tr td,
+    tbody tr td p,
+    tbody tr td img {
+        transition: .2s ease-in-out;
+    }
+
+    tbody tr.hide td,
+    tbody tr.hide td p {
+        padding: 0;
+        font: 0 / 0 sans-serif;
+        transition: .2s ease-in-out .5s;
+    }
+
+    tbody tr.hide td img {
+        width: 0;
+        height: 0;
+        transition: .2s ease-in-out .5s;
+    }
+        /* body {
             background-color: #f8f9fa;
             font-family: Arial, sans-serif;
         }
@@ -30,11 +195,10 @@
         th {
             background-color: #343a40;
             color: white;
-        }
+        } */
 
         form {
             color: black;
-            margin: auto;
             width: 500px;
             padding: 20px;
             border: 1px solid #dee2e6;
@@ -93,44 +257,57 @@
     </style>
     </head>
     <body>
-    <table>
-        <th>
-            Submission
-        </th>
-        <th>
-            Presentation Group
-        </th>
-        @foreach($submission as $submission)
-            @if($submission->submissionType != 'Publication ONLY')
-            <tr>
-                <td>
-                    <a style="font-size:20px; font-weight:bold; color:black;  text-decoration:none;" href="#" class="submission-code" data-submission-code="{{$submission->submissionCode}}" data-submission-type="{{$submission->submissionType}}" data-submission-title="{{$submission->submissionTitle}}" data-submission-type="{{$submission->submissionTitle}}" data-sub-theme="{{$submission->subTheme}}" data-present-mode="{{$submission->presentMode}}">
-                        <div class="submissionCode">
-                            {{$submission->submissionCode}}
-                        </div>
-                    </a>
-                </td>
-                <td>
-                    @if($submission->presentationGroup == null)
-                        <p>Current Group : </p> <h5 style="color: black;">Not Assigned</h5>
-                    @else
-                        <p>Current Group : </p> <h5 style="color: black;">{{ $submission->presentationGroup }}</h5>
-                    @endif
-                    <form method="post" action="{{ route('editSubmissionPresentationGroup', ['submissionCode' => $submission->submissionCode]) }}">
-                        @csrf
-                        <select name="group" id="group">
-                            <option value="Not Assigned">Not Assigned</option>
-                            @foreach($schedule as $thisSchedule)
-                                <option value="{{ $thisSchedule->presentationGroup }}">{{ $thisSchedule->presentationGroup }}</option>
-                            @endforeach
-                        </select>  <br>
-                        <button>Save Change</button>
-                    </form>
-                </td>
-            </tr>
-            @endif
-        @endforeach
-    </table>
+    <main class="table">
+        <section class="table__header">
+            <h1>Presentation Arrangement</h1>
+        </section>
+        <section class="table__body">
+            <table>
+                <thead>
+                <tr>
+                    <th>
+                        Submission
+                    </th>
+                    <th>
+                        Presentation Group
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach($submission as $submission)
+                        @if($submission->submissionType != 'Publication ONLY')
+                        <tr>
+                            <td>
+                                <a style="font-size:20px; font-weight:bold; color:black;  text-decoration:none;" href="#" class="submission-code" data-submission-code="{{$submission->submissionCode}}" data-submission-type="{{$submission->submissionType}}" data-submission-title="{{$submission->submissionTitle}}" data-submission-type="{{$submission->submissionTitle}}" data-sub-theme="{{$submission->subTheme}}" data-present-mode="{{$submission->presentMode}}">
+                                    <div class="submissionCode">
+                                        {{$submission->submissionCode}}
+                                    </div>
+                                </a>
+                            </td>
+                            <td>
+                                @if($submission->presentationGroup == null)
+                                    <p>Current Group : </p> <h5 style="color: black;">Not Assigned</h5>
+                                @else
+                                    <p>Current Group : </p> <h5 style="color: black;">{{ $submission->presentationGroup }}</h5>
+                                @endif
+                                <form method="post" action="{{ route('editSubmissionPresentationGroup', ['submissionCode' => $submission->submissionCode]) }}">
+                                    @csrf
+                                    <select name="group" id="group">
+                                        <option value="Not Assigned">Not Assigned</option>
+                                        @foreach($schedule as $thisSchedule)
+                                            <option value="{{ $thisSchedule->presentationGroup }}">{{ $thisSchedule->presentationGroup }}</option>
+                                        @endforeach
+                                    </select>  <br>
+                                    <button>Save Change</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endif
+                    @endforeach
+                </tbody>
+            </table>
+        </section>
+    </main>
     <script>
         // Get all submission code links
         const submissionCodeLinks = document.querySelectorAll('.submission-code');
