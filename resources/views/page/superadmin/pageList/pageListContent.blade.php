47c0c012cef05f55a36572b3842afa3abfe1007a
chew0420 <chewjiawei123@gmail.com>
2023-09-22 17:50:55 +0800

    22/9

--- resources/views/page/superadmin/pageList/pageListContent.blade.php
+++ resources/views/page/superadmin/pageList/pageListContent.blade.php
@@ -3,15 +3,44 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
 <!-- Core theme CSS (includes Bootstrap)-->
 <link href="css/styles.css" rel="stylesheet" />
-<style>
-    .abc{
-        flex-wrap: wrap;
-    }
-</style>
-<div style="text-align: center; background-color: white; height:100%; display:flex; overflow:auto; flex-wrap: wrap;">
-    @foreach($pages as $page)
-        @if($page->editable == true)
-        <a href="{{ route('editPage', ['page' => $page->pageName]) }}" id="page" style="margin:auto;width:300px ; background-color: blue; color: white; text-decoration: none; margin-top: 20px; padding: 10px; border-radius: 20px; display: block;">{{ $page->pageName }}</a><br>
-        @endif
-    @endforeach
+<div style="text-align: center; background-color: white; height: 100%; display: flex; overflow: auto; flex-wrap: wrap;">
+    <div style="margin: auto; max-width: 800px; padding: 20px; border: 1px solid #ccc; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); background-color: #f5f5f5;">
+        <style>
+            .button-container {
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: center;
+                gap: 10px;
+            }
+
+            #button {
+                margin: 5px;
+                width: 300px;
+                height: 50px;
+                background: #0d6efd;
+                border-radius: 40px;
+                color: #fff;
+                font-size: 16px;
+                border: none;
+                outline: none;
+                box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+                cursor: pointer;
+                transition: .3s ease-in-out;
+            }
+
+            #button:hover {
+                background: #0056b3;
+            }
+        </style>
+
+        <div class="button-container">
+            @foreach($pages as $page)
+                @if($page->editable == true)
+                    <a href="{{ route('editPage', ['page' => $page->pageName]) }}" id="page">
+                        <button id="button">{{ $page->pageName }}</button>
+                    </a>
+                @endif
+            @endforeach
+        </div>
+    </div>
 </div>
