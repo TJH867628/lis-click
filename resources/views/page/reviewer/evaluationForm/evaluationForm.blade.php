f9a54a66280f90e026711fed56025bd847259675
chaiyuxuan666 <128983394+chaiyuxuan666@users.noreply.github.com>
2023-10-08 12:33:02 +0800

    update reviewer page

--- resources/views/page/reviewer/evaluationForm/evaluationForm.blade.php
+++ resources/views/page/reviewer/evaluationForm/evaluationForm.blade.php
@@ -10,6 +10,7 @@
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/styles.css" rel="stylesheet" />
         
