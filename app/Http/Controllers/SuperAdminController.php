3f0127b6341f3593f34a0fb022f21e03bf4110b1
chew0420 <chewjiawei123@gmail.com>
2023-11-06 15:45:35 +0800

    6/11

--- app/Http/Controllers/SuperAdminController.php
+++ app/Http/Controllers/SuperAdminController.php
@@ -126,4 +126,14 @@ class SuperAdminController extends Controller
             return redirect('login')->with('fail', 'Login Session Expire, Please Login again');
         }
     }
+
+    public function checksubmission(){
+        if(session()->has("LoggedSuperAdmin")){
+            session()->start();
+            $checksubmission  = tbl_submission::all();
+            return view('page.superadmin.homePage.homePage(SuperAdmin)',['checksubmission'=>$checksubmission]);
+        }else{
+            return redirect('login')->with('fail','Login Session Expire,Please Login again');
+        }
+    }
 }
--- resources/views/page/superadmin/homePage/homePageContent(SuperAdmin).blade.php
+++ resources/views/page/superadmin/homePage/homePageContent(SuperAdmin).blade.php
@@ -146,24 +146,28 @@
                       <table class="table">
                         <thead>
                           <tr>
-                            <th> # </th>
                             <th> Name </th>
-                            <th> Due Date </th>
+                            <th> Reviewer </th>
                             <th> Progress </th>
                           </tr>
                         </thead>
+                        @if(isset($checksubmission))
                         <tbody>
+                          @foreach($checksubmission as $checksubmission)
                           <tr>
-                            <td> 1 </td>
-                            <td> Herman Beck </td>
-                            <td> May 15, 2015 </td>
-                            <td>
-                              <div class="progress">
-                                <div class="progress-bar bg-gradient-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                              </div>
-                            </td>
+                            <td>{{ $checksubmission->submissionCode }}</td>
+                            <td>{{ $checksubmission->reviewerID }}</td>
+                            <td>{{ $checksubmission->reviewStatus}}</td>
                           </tr>
-                          <tr>
+                          @endforeach
+                          @else
+                            <tr style="color: black;">
+                              <td colspan="8">
+                                No record found.
+                              </td>
+                            </tr>
+                          @endif
+                          <!-- <tr>
                             <td> 2 </td>
                             <td> Messsy Adam </td>
                             <td> Jul 01, 2015 </td>
@@ -172,47 +176,8 @@
                                 <div class="progress-bar bg-gradient-danger" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
                               </div>
                             </td>
-                          </tr>
-                          <tr>
-                            <td> 3 </td>
-                            <td> John Richards </td>
-                            <td> Apr 12, 2015 </td>
-                            <td>
-                              <div class="progress">
-                                <div class="progress-bar bg-gradient-warning" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                              </div>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td> 4 </td>
-                            <td> Peter Meggik </td>
-                            <td> May 15, 2015 </td>
-                            <td>
-                              <div class="progress">
-                                <div class="progress-bar bg-gradient-primary" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                              </div>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td> 5 </td>
-                            <td> Edward </td>
-                            <td> May 03, 2015 </td>
-                            <td>
-                              <div class="progress">
-                                <div class="progress-bar bg-gradient-danger" role="progressbar" style="width: 35%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
-                              </div>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td> 5 </td>
-                            <td> Ronald </td>
-                            <td> Jun 05, 2015 </td>
-                            <td>
-                              <div class="progress">
-                                <div class="progress-bar bg-gradient-info" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
-                              </div>
-                            </td>
-                          </tr>
+                          </tr> -->
+                          
                         </tbody>
                       </table>
                     </div>
