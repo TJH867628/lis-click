2ee2b6a3c0ca7dfddba0edc2b66420093e0333e1
TJH <jinghengtan762@gmail.com>
2023-11-17 19:01:05 +0800

    Update FaqController.php

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -72,8 +72,7 @@ class FaqController extends Controller
         $userMessage = (string) $userMessage; // Convert $message to a string
         $officialEmail = tbl_masterdata::where('masterdata_name','officialEmail')->first();
         $email = new contactUs($name,$userEmail,$subject,$userMessage);
-        $email->subject("Contact Us Email From User");
-        $email->from($userEmail);
+        $email->from($userEmail, $name);
         Mail::to($officialEmail->masterdata_value)->send($email);
 
         return redirect()->back()->with('success','Your message has been submitted successfully');
