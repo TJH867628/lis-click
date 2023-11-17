5745316c8f0974fc8529931eb9d2cc953a32f875
TJH <jinghengtan762@gmail.com>
2023-11-17 12:52:11 +0800

    debug

--- resources/views/page/visitor/forgotPassword/confirmOTP.blade.php
+++ resources/views/page/visitor/forgotPassword/confirmOTP.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Gallery</title>
+        <title>Forgot Password</title>
         <!-- Favicon-->
             <link rel="icon" type="image/x-icon" href="{{ $favicon }}" />
 
